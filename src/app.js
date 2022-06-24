2563ab2fc5ce5cd54cd77aaba726eec367a77c8f
Hayder10 <rodrigo.cabrera.catemu10@gmail.com>
2022-06-24 14:49:45 +0000

    First iteration of domain generator in JS

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".net", ".us", ".io"];
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        for (let e of extension) {
+          console.log(p + a + n + e);
+        }
+      }
+    }
+  }
 };
